a506091198b92f6b2bb2a0d3db5e8afc6c3d7b9a
Ribeiro1300 <jhonatan_dribeiro@hotmail.com>
2021-10-24 15:11:45 -0300

    trying to deploy on heroku

--- src/database/database.js
+++ src/database/database.js
@@ -3,11 +3,11 @@ import pg from "pg";
 const { Pool } = pg;
 
 const connection = new Pool({
-  host: "localhost",
+  host: "ec2-52-0-234-93.compute-1.amazonaws.com",
   port: 5432,
-  user: "postgres",
-  password: "123456",
-  database: "my_wallet",
+  user: "lwgpjxpzkeucvb",
+  password: "345344ef1009a718d17398a554ee3198914472bab818d33214c75e02b5ca3e1e",
+  database: "heroku pg:psql postgresql-pointy-02147 --app my-wallet-projeto14",
 });
 
 export default connection;
