66fa25f480a4f909c82f36f2f42022ee1b9a4edb
Ribeiro1300 <jhonatan_dribeiro@hotmail.com>
2021-10-28 20:12:31 -0300

    deploy

--- src/database/database.js
+++ src/database/database.js
@@ -3,11 +3,8 @@ import pg from "pg";
 const { Pool } = pg;
 
 const connection = new Pool({
-  host: "ec2-52-0-234-93.compute-1.amazonaws.com",
-  port: 5432,
-  user: "lwgpjxpzkeucvb",
-  password: "345344ef1009a718d17398a554ee3198914472bab818d33214c75e02b5ca3e1e",
-  database: "heroku pg:psql postgresql-pointy-02147 --app my-wallet-projeto14",
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
 });
 
 export default connection;
